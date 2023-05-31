ce48cbb8ff6e3db574811325273390505be9235a
Svetlana <medjog@gmail.com>
2023-05-31 11:47:33 +0400

    Решение 2 задачи, начало

--- Num02.py
+++ Num02.py
@@ -5,3 +5,21 @@
 # 5
 # 1 2 3 4 5
 # 6 -> 5
+
+my_list = []
+num = int(input('Введите количество элементов списка: '))
+for i in range(num):
+    element = int(input('Введите число: '))
+    my_list.append(element)
+number = int(input('Введите число: '))
+print(my_list)
+my_list2 = []
+for i in range(len(my_list)):
+    element2 = number - my_list[i] 
+    #print(element2)
+    my_list2.append(abs(element2))
+print(my_list2)
+print(min(my_list2))
+
+
+    
