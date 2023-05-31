1be8e9380976390fe85f76e4d857e200eb5a3bfc
Svetlana <medjog@gmail.com>
2023-05-31 12:47:10 +0400

    Решение 2 задачи, два варианта

--- Num02.py
+++ Num02.py
@@ -9,17 +9,31 @@
 my_list = []
 num = int(input('Введите количество элементов списка: '))
 for i in range(num):
-    element = int(input('Введите число: '))
+    element = int(input('Введите число: ')) # элементы списка
     my_list.append(element)
-number = int(input('Введите число: '))
+number = int(input('Введите число: ')) # заданное число Х
 print(my_list)
 my_list2 = []
 for i in range(len(my_list)):
-    element2 = number - my_list[i] 
-    #print(element2)
+    element2 = number - my_list[i] # разница между заданным числом и элементами списка
     my_list2.append(abs(element2))
-print(my_list2)
-print(min(my_list2))
+min_element2 = min(my_list2) # минимальный элемент второго списка
+min_index = my_list2.index(min_element2) # индекс минимального элемента 2 списка соответсвует индексу наиболее близкого числа из 1 списка
+print(f'Самый близкий по значению элемент к заданному числу {number} -> {my_list[min_index]}.')
 
+from random import randint
+list_1 = []
+number = int(input('Введите количество элементов списка: '))
+for i in range(number):
+    list_1.append(randint(-10, 10))
+print(list_1)
+element = int(input('Введите число: '))
+list_2 = []
+for i in range(len(my_list)):
+    element2 = element - list_1[i] # разница между заданным числом и элементами списка
+    list_2.append(abs(element2))
+min_element2 = min(list_2) # минимальный элемент второго списка
+min_index = list_2.index(min_element2) # индекс минимального элемента 2 списка соответсвует индексу наиболее близкого числа из 1 списка
+print(f'Самый близкий по значению элемент к заданному числу {element} -> {list_1[min_index]}.')
 
     
